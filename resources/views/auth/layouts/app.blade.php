0f59d780ace7d0a7da483fddac6ccec3de49d617
sigit girianto <giriantosigit@gmail.com>
2017-09-07 15:19:35 +0700

    add layouts.app for auth

---
+++ resources/views/auth/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <div id="fixed">
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+            <title>{{ config('app.name', 'Ravellog') }}</title>
+            <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+            <meta name="description" content="">
+            <meta name="author" content="">
+            <!-- Bootstrap Core CSS -->
+            <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+            <!-- Custom CSS -->
+            <link href="{{ asset('css/jquery-ui.css') }}" rel="stylesheet">
+            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+            <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
+            <!-- jQuery -->
+            <script src="{{ asset('js/jquery.js') }}"></script>
+            <script src="{{ asset('js/jquery-ui.js') }}"></script>
+        </head>
+
+        <body>
+            <div class="content">
+                <div class="head-content">
+                    Ravellog
+                </div>
+                @yield('content')
+            </div>
+        </body>
+    </div>
+</html>
