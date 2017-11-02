fd1a3156c3dfd6f7868cca6813b8e41427b7b219
sigitgirianto <giriantosigit@gmail.com>
2017-11-02 11:52:38 +0700

    add filter to retrieve data

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -7,12 +7,29 @@ use DB;
 
 class ReportController extends Controller
 {
-    public function inbound(){
-    	$boxes = DB::table('inbound_boxes')
-    	->where('arrival_destination',auth()->user()->privilege)
-    	->join('boxes','inbound_boxes.box_id','=','boxes.id')
-    	->join('employees','inbound_boxes.employee_id','=','employees.id')
-       	->paginate(10);
+    public function inbound(Request $request){
+    	if ($request->has(['fromDate','toDate'])){
+    		$fromDate = $request->fromDate;
+            $toDate = $request->toDate;
+            
+            //to include data on the day of the deadline
+            $endDate = $request->toDate.' '.'23:59:59';
+
+    		$boxes = DB::table('inbound_boxes')
+	    	->where('arrival_destination',auth()->user()->privilege)
+	    	->join('boxes','inbound_boxes.box_id','=','boxes.id')
+	    	->join('employees','inbound_boxes.employee_id','=','employees.id')
+	    	->whereBetween('inbound_boxes.created_at',[$fromDate,$endDate])->paginate(10)
+	    	->appends('fromDate',request('fromDate'))
+	    	->appends('toDate',request('toDate'));
+
+    	}else{
+	    	$boxes = DB::table('inbound_boxes')
+	    	->where('arrival_destination',auth()->user()->privilege)
+	    	->join('boxes','inbound_boxes.box_id','=','boxes.id')
+	    	->join('employees','inbound_boxes.employee_id','=','employees.id')
+	       	->paginate(10);
+    	}
     	return view('report.inbound',compact('boxes'));
     }
 }
--- resources/views/report/inbound.blade.php
+++ resources/views/report/inbound.blade.php
@@ -6,49 +6,69 @@
         <div class="col-md-10 col-md-offset-1">
         <legend style="padding-left:1em; padding-top:0.5em;">Shipping In
         	<span id="control-panel">
-        	  <a title="Export as pdf" data-toggle="modal" data-target="#exportModal"><button class="btn btn-info btn-sm">Export</button></a>
+        	  <a title="Export as pdf" data-toggle="modal" data-target="#exportModal"><button class="btn btn-success btn-sm">Export</button></a>
         	</span>
         </legend>
         @if (Session::has('message'))
           <div class="alert alert-info">{{ Session::get('message') }}</div>
         @endif
-            <div class="panel panel-success">
-                    @if(Auth::check())
-                    
-                    {{ csrf_field() }}
-                      <fieldset>
-                        <div class="col-md-12">
-                          <table class="table">
-                            <thead>
-                              <tr>
-                                <th>Box Tag</th>
-                                <th>Box Name</th>
-                                <th>Arrival Date</th>
-                                <th>Person in Charge</th>
-                              </tr>
-                            </thead>
 
-                            <tbody>
-                              @foreach($boxes as $box)
-                                <tr>
-                                  <td>{{$box->tag_tag}}</td>
-                                  <td>{{$box->name}}</td>
-                                  <td>{{$box->exp_arrival_date}}</td>
-                                  <td>{{$box->employee_name}}</td>
-                                </tr>
-                              @endforeach
-                            </tbody>
-                          </table>
-
-                          <div class="text-right">
-                            {!! $boxes->links(); !!}
-                          </div>    
-                        </div>
-                      </fieldset>
-                    
-                    @endif
-
+        @if(Auth::check())
+        <!-- Filter to retrieve data from database -->
+        <form role="form" action="{{route('reportInbound')}}" method="GET">
+          <fieldset>
+            <div class="col-sm-3">
+              <div class="form-group">
+                <input type="text" name="fromDate" id="datepicker1" class="form-control" placeholder="From" placeholder="From" required>
+              </div>
             </div>
+            <div class="col-sm-3">
+              <div class="form-group">
+                <input type="text" name="toDate" id="datepicker2" class="form-control" placeholder="To" placeholder="To" required>
+              </div>
+            </div>
+            <div class="col-sm-1">  
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary">Filter</button>
+              </div>
+          </div>
+         </fieldset>
+        </form>
+
+          <div class="panel panel-success">
+            {{ csrf_field() }}
+              <fieldset>
+                <div class="col-md-12">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th>Box Tag</th>
+                        <th>Box Name</th>
+                        <th>Arrival Date</th>
+                        <th>Person in Charge</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      @foreach($boxes as $box)
+                        <tr>
+                          <td>{{$box->tag_tag}}</td>
+                          <td>{{$box->name}}</td>
+                          <td>{{$box->exp_arrival_date}}</td>
+                          <td>{{$box->employee_name}}</td>
+                        </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+
+                  <div class="text-right">
+                    {!! $boxes->links(); !!}
+                  </div>    
+                </div>
+              </fieldset>
+            </div>
+            @endif
+
             @if(Auth::guest())
               <a href="/login" class="btn btn-warning">You need to login to access this page</a>
             @endif
@@ -64,8 +84,8 @@
 	        <h4 class="modal-title" id="myModalLabel">Report Range
 	      </div>
 	      <div class="modal-body">
-	        <fieldset>
-	          <form role="form" action="{{route('pdfInbound')}}" method="GET">
+          <form role="form" action="{{route('pdfInbound')}}" method="GET">
+            <fieldset>
 	            <div class="col-sm-5">
 	              <div class="form-group">
 	                <input type="text" name="fromDate" id="datepicker1" class="form-control" placeholder="From" placeholder="From" required>
@@ -78,11 +98,11 @@
 	            </div>
 	            <div class="col-sm-2">	
 		            <div class="form-group">
-		              <button type="submit" class="btn btn-primary">Export</button>
+		              <button type="submit" class="btn btn-success">Export</button>
 		            </div>
 		        </div>
-	          </form>
-	        </fieldset>
+	         </fieldset>
+          </form>
 	      </div>
 	    </div>
 	  </div>
